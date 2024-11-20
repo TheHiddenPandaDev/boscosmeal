56fd760b368d2cc98205bf753889ed1174faa0b6
Dani Roman <droman@hubos.com>
2024-11-20 09:56:37 +0100

    Allow update database in local

--- backup_site.sh
+++ backup_site.sh
@@ -14,13 +14,23 @@ if [ ! -d "wp-content" ]; then
     exit 1
 fi
 
-# Export the database
-echo "Exporting database..."
+# Export the database from the remote server
+echo "Exporting database from the remote server..."
 mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" > "./dump.sql"
 if [ $? -eq 0 ]; then
     echo "Database exported to ./dump.sql"
 else
     echo "Error exporting the database" >&2
+    exit 1
+fi
+
+# Import the database to the local MySQL server
+echo "Importing database to the local MySQL server..."
+mysql -h "$LOCAL_DB_HOST" -u "$LOCAL_DB_USER" -p"$LOCAL_DB_PASS" "$LOCAL_DB_NAME" < "./dump.sql"
+if [ $? -eq 0 ]; then
+    echo "Database successfully imported to the local server."
+else
+    echo "Error importing the database to the local server" >&2
     exit 1
 fi
 
@@ -46,8 +56,8 @@ if [ -f "./wp-config.php" ]; then
 fi
 
 # Replace domain in the database using SQL, considering the prefix
-echo "Replacing domain in the database..."
-mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" <<SQL
+echo "Replacing domain in the local database..."
+mysql -h "$LOCAL_DB_HOST" -u "$LOCAL_DB_USER" -p"$LOCAL_DB_PASS" "$LOCAL_DB_NAME" <<SQL
 UPDATE ${WP_DB_PREFIX}options
 SET option_value = REPLACE(option_value, '$OLD_DOMAIN', '$NEW_DOMAIN')
 WHERE option_name = 'home' OR option_name = 'siteurl';
@@ -63,9 +73,9 @@ SET meta_value = REPLACE(meta_value, '$OLD_DOMAIN', '$NEW_DOMAIN');
 SQL
 
 if [ $? -eq 0 ]; then
-    echo "Domain replacement in the database completed."
+    echo "Domain replacement in the local database completed."
 else
-    echo "Error replacing the domain in the database." >&2
+    echo "Error replacing the domain in the local database." >&2
     exit 1
 fi
 
